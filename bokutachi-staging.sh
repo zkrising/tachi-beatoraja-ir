f625f616b826039d4c26b2ffe9cba56ec6ccc176
zkldi <20380519+zkldi@users.noreply.github.com>
2021-12-03 17:10:49 +0000

    bump version to 2.0.1

--- bokutachi-staging.sh
+++ bokutachi-staging.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-TCHIR_HOME=https://staging.bokutachi.xyz TCHIR_NAME="Bokutachi Staging IR" TCHIR_BASE_URL="https://staging.bokutachi.xyz" TCHIR_VERSION="v2.0.0" ./build.sh
+TCHIR_HOME=https://staging.bokutachi.xyz TCHIR_NAME="Bokutachi Staging IR" TCHIR_BASE_URL="https://staging.bokutachi.xyz" TCHIR_VERSION="v2.0.1" ./build.sh
--- bokutachi.sh
+++ bokutachi.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-TCHIR_HOME=https://bokutachi.xyz TCHIR_NAME="Bokutachi IR" TCHIR_BASE_URL="https://bokutachi.xyz" TCHIR_VERSION="v2.0.0" ./build.sh
+TCHIR_HOME=https://bokutachi.xyz TCHIR_NAME="Bokutachi IR" TCHIR_BASE_URL="https://bokutachi.xyz" TCHIR_VERSION="v2.0.1" ./build.sh
